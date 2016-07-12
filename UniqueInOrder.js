4f32095aeb3d3198b6c6f5990b4174d1b26d3595
jaimielogan <jaimielogan@gmail.com>
2016-07-12 17:51:05 -0600

    Initial Commit

---
+++ UniqueInOrder.js
@@ -0,0 +1,12 @@
+//Implement the function unique_in_order which takes as argument a sequence and returns a list of items without any elements with the same value next to each other and preserving the original order of elements.
+
+var uniqueInOrder=function(iterable){
+  //your code here - remember iterable can be a string or an array
+  var output = [];
+  for (i=0; i<iterable.length; i++){
+    if(iterable[i] !== iterable[i+1]){
+    output.push(iterable[i]);
+    }
+  }
+  return output;
+};
