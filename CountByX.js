6b71ad3d0a700d8d63ca758daef1c65918bdd314
jaimielogan <jaimielogan@gmail.com>
2016-07-12 18:10:25 -0600

    Initial Commit

---
+++ CountByX.js
@@ -0,0 +1,9 @@
+//Create a function with two arguments that will return a list of length (n) with multiples of (x).
+
+function countBy(x, n) {
+  var z = [];
+  for(i=1; i<=n; i++){
+    z.push(i*x);
+  }
+  return z;
+}
